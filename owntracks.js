06ca56c2de59f9030236202c964b992619f4b239
Ben Hardill <hardillb@gmail.com>
2016-04-26 10:29:58 +0100

    Added a bunch of error handling and some auto conversion of string input.

--- owntracks.js
+++ owntracks.js
@@ -14,45 +14,68 @@
  * limitations under the License.
  **/
 module.exports = function(RED) {
-	"use strict";
+    "use strict";
 
-	var sodium = require('libsodium-wrappers');
+    var sodium = require('libsodium-wrappers');
 
-	var owntracks = function(n) {
-		RED.nodes.createNode(this,n);
-		var node = this;
-		var secret = this.credentials.secret;
+    var owntracks = function(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var secret = this.credentials.secret;
 
-		this.on('input',function(msg){
-			if (msg.payload._type === 'encrypted') {
-				if (secret) {
-					var cypherText = new Buffer(msg.payload.data, 'base64');
-					var nonce = cypherText.slice(0,24);
-	      			var key = new Buffer(32);
-	      			key.fill(0);
-	      			key.write(secret);
-	      			var clearText = sodium.crypto_secretbox_open_easy(cypherText.slice(24),nonce,key,"text");
-	      			var clearObj = JSON.parse(clearText);
-	      			msg.payload = clearObj;
-      			} else {
-      				node.status({fill:'red',shape:'dot',text:'No secret'});
-      				node.send(msg);
-      				return;
-      			}
-			}
-			if (msg.payload._type === 'location') {
-				msg.location = {
-					lat: msg.payload.lat,
-					lon: msg.payload.lon
-				};
-			}
+        this.on('input',function(msg){
+            if (typeof msg.payload === 'string') {
+                try {
+                    msg.payload = JSON.parse(msg.payload);
+                } catch (e) {
+                    node.warn("String payload is not JSON");
+                    return;
+                }
+            }
 
-			node.send(msg);
+            if (msg.payload._type) {
+                if (msg.payload._type === 'encrypted') {
+                    if (secret) {
+                        var cypherText = new Buffer(msg.payload.data, 'base64');
+                        var nonce = cypherText.slice(0,24);
+                        var key = new Buffer(32);
+                        key.fill(0);
+                        key.write(secret);
+                        try {
+                            var clearText = sodium.crypto_secretbox_open_easy(cypherText.slice(24),nonce,key,"text");
+                        } catch (e) {
+                            node.warn("error decrypting payload");
+                            return;
+                        }
+                        try{
+                            var clearObj = JSON.parse(clearText);
+                        } catch (e) {
+                            node.warn("decrypted message not JSON");
+                            return;
+                        }
+                        msg.payload = clearObj;
+                    } else {
+                        node.status({fill:'red',shape:'dot',text:'No secret'});
+                        node.send(msg);
+                        return;
+                    }
+                }
+                if (msg.payload._type === 'location') {
+                    msg.location = {
+                        lat: msg.payload.lat,
+                        lon: msg.payload.lon,
+                        name: msg.payload.tid
+                    };
+                    msg.payload.name = msg.payload.name || msg.payload.tid;
+                }
+            }
 
-		});
+            node.send(msg);
 
-	};
-	RED.nodes.registerType("owntracks", owntracks, { credentials: {
-		secret: {type: 'text'}
-	}});
+        });
+
+    };
+    RED.nodes.registerType("owntracks", owntracks, { credentials: {
+        secret: {type: 'text'}
+    }});
 }
